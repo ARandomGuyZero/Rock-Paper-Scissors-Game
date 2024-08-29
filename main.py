33ffb3c76bc39d2e40920a03df551d5a6b7c9b53
Alan Ramos López <118926925+ARandomGuyZero@users.noreply.github.com>
2024-08-28 17:51:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 """
 Rock, Paper, Scissors Game
 
+Author: Alan
+Date: August 22nd 2024
+
 This script is a simple implementation of the classic Rock, Paper, Scissors game. 
 The player chooses between Rock, Paper, or Scissors, and the computer randomly selects one as well. 
 The game then compares the choices and determines the winner.
